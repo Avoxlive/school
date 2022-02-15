9e55f9ce5ff2a511af2af33c6d6d88c2cdeb9fe7
Avoxlive <avox.live@gmail.com>
2022-02-15 11:42:34 +0530

    commit

---
+++ application/views/backend/page_info.php
@@ -0,0 +1,15 @@
+<!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row bg-title" style="background: fof;">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <h4 class="page-title"><?php echo $page_title;?></h4>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12"> <a href="" target="_blank" class="btn btn-info btn-sm pull-right m-l-20 btn-rounded hidden-xs hidden-sm waves-effect waves-light"><i class="fa fa-globe" aria-hidden="true" style="padding: 0.5rem;"></i>Hospital website</a>
+                        <ol class="breadcrumb">
+                            <li><a href=""><?php echo $system_name;?></a></li>
+                            <li class="active"><?php echo date('d M,Y');?></li>
+                        </ol>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
